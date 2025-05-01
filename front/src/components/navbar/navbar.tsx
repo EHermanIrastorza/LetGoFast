551e7b20105920a237cb3443c9f788a41f5699b6
EmmaInverse <emma225h@gmail.com>
2025-05-01 01:12:11 -0300

    arrancando el front

---
+++ front/src/components/navbar/navbar.tsx
@@ -0,0 +1,8 @@
+export default function Navbar() {
+  return (
+    <div>
+      <h1>Navbar</h1>
+      <p>probando la font de google </p>
+    </div>
+  );
+}
---
+++ front/src/interface/productInterface.ts
@@ -0,0 +1,11 @@
+export interface ProductInterface {
+  id: string;
+  productName: string;
+  productDescription: string;
+  productPrice: number;
+  productImage: string[];
+  productModel: string;
+  productSpeed: string;
+  productGear: string;
+  productStyle: string;
+}
